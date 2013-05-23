5ce8b3af08e35cc28968123360d97e851e587387
David Leuliette <dleuliette@gmail.com>
2013-05-23 13:59:04 +0200

    add gem middleman

--- guidecss/config.rb
+++ guidecss/config.rb
@@ -58,23 +58,15 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
 end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.remote = 'gh-pages'
+  deploy.branch = 'master'
+end
+
+activate :livereload
