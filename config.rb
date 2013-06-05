5e294cd2246c69cc785060b5a11f842039b30ed8
David Leuliette <dleuliette@gmail.com>
2013-06-05 13:52:07 +0200

    new structure

---
+++ config.rb
@@ -0,0 +1,72 @@
+###
+# markdown
+###
+require 'maruku'
+
+###
+# Compass
+###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+#activate :livereload
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.remote = 'gh-pages'
+  deploy.branch = 'gh-pages'
+end
